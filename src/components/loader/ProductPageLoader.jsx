2d02028bd1af4f2994af631449f92e05c7b0938a
Daniel <danielkore1234@gmail.com>
2024-03-08 11:40:03 +0100

    Added skeleton loader

--- src/components/loader/ProductPageLoader.jsx
+++ src/components/loader/ProductPageLoader.jsx
@@ -4,13 +4,13 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 const ProductPageLoader = () => {
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-3 gap-7 '>
-                <Skeleton className='rounded-lg  ' width={325} height={390} />
-                <Skeleton className='rounded-lg  ' width={325} height={390} />
-                <Skeleton className='rounded-lg  ' width={325} height={390} />
-                <Skeleton className='rounded-lg  ' width={325} height={390} />
-                <Skeleton className='rounded-lg  ' width={325} height={390} />
-                <Skeleton className='rounded-lg  ' width={325} height={390} />
+    <div className='grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 gap-7 '>
+                <Skeleton className='rounded-lg w-full lg:w-[210px]  xl:w-[300px] h-[370px] ' />
+                <Skeleton className='rounded-lg  w-full lg:w-[210px]  xl:w-[300px] h-[370px]' />
+                <Skeleton className='rounded-lg w-full lg:w-[210px]  xl:w-[300px] h-[370px] ' />
+                <Skeleton className='rounded-lg w-full lg:w-[210px]  xl:w-[300px] h-[370px] ' />
+                <Skeleton className='rounded-lg w-full lg:w-[210px]  xl:w-[300px] h-[370px] ' />
+                <Skeleton className='rounded-lg w-full lg:w-[210px]  xl:w-[300px] h-[370px] ' />
             </div>
   )
 }
